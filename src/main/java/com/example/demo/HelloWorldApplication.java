31d50601300cabe718ce47fd6bb2c6b48f55f77d
TaoufykMohamed <taoufyk.lydex.2024@gmail.com>
2026-07-23 20:00:25 +0100

    Remove test webhook endpoint
    Removed test webhook endpoint from HelloWorldApplication.

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -13,12 +13,7 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-	//test
-	@GetMapping("/test-webhook")
-	public String testWebhook() {
-		return "Le webhook fonctionne parfaitement !";
-	}
-
+	
 	// Cette annotation indique que les requêtes HTTP GET sur la racine ("/")
 	// déclencheront cette méthode
 	@GetMapping("/")
